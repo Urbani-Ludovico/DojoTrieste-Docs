9cb75826fbf560db9679ac4685e408a93818978f
Urbani Ludovico <lu.urbani@outlook.com>
2025-10-09 17:15:25 +0200

    Changed class name added djtsdoc for receipt

--- .class/install.sh
+++ .class/install.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/texmf/tex/latex/djts/
 mkdir -p ~/.config/texstudio/completion/user/
 
-cp -r djts.cls djtsdoc.cls djtsmod.cls images ~/texmf/tex/latex/djts/
-cp djts.cwl djtsdoc.cwl djtsmod.cwl ~/.config/texstudio/completion/user/
+cp -r djts.cls djtsdoc.cls djtsasddoc.cls djtsmod.cls images ~/texmf/tex/latex/djts/
+cp djts.cwl djtsdoc.cwl djtsasddoc.cwl djtsmod.cwl ~/.config/texstudio/completion/user/
 
 texhash ~/texmf/
